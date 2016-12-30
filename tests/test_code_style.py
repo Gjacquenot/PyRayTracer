9ed753b8b50d73c946cb572fade3a633ca1e677f
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2016-12-30 15:20:56 +0100

    [master] Added a Python code check style

---
+++ tests/test_code_style.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import unittest
+import pep8
+
+import os.path
+
+class TestCodeStyle(unittest.TestCase):
+
+    def test_pep8_conformance(self):
+        tests_dir = os.path.dirname(__file__)
+        modules_files = os.path.abspath(os.path.join(tests_dir, "..", "raytracer.py"))
+        pep8style = pep8.StyleGuide(max_line_length=200, ignore=['E731'])
+        result = pep8style.check_files([modules_files])
+        self.assertEqual(result.total_errors,
+                         0,
+                         "Found pep8 conformance issues",
+                         )
+
+if __name__ == '__main__':
+    unittest.main()

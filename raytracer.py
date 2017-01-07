7412602bb3552a86ca7f175ef6447fe3e19def99
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2017-01-07 18:35:18 +0100

    [master] Added functions present in raytrace.py

--- raytracer.py
+++ raytracer.py
@@ -15,7 +15,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-RayCastLength = 100
+RayCastLength = 2000
 ColorRay = [1.0, 1.0, 0.0]
 ColorRayMiss = [1.0, 1.0, 1.0]
 
@@ -612,6 +612,126 @@ def reflect(ren, appendFilter, surface1, surface2):
     ren.AddActor(glyphsa)
     '''
     return intersection_points, normalsCalcSurface2, reflect_polydata
+
+
+def is_same_transform(matrix0, matrix1):
+    """Return True if two matrices perform same transformation.
+
+    >>> is_same_transform(np.identity(4), np.identity(4))
+    True
+    >>> is_same_transform(np.identity(4), random_rotation_matrix())
+    False
+
+    """
+    matrix0 = np.array(matrix0, dtype=np.float64, copy=True)
+    matrix0 /= matrix0[3, 3]
+    matrix1 = np.array(matrix1, dtype=np.float64, copy=True)
+    matrix1 /= matrix1[3, 3]
+    return np.allclose(matrix0, matrix1)
+
+
+def unit_vector(data, axis=None, out=None):
+    """Return ndarray normalized by length, i.e. Euclidean norm, along axis.
+
+    >>> v0 = np.random.random(3)
+    >>> v1 = unit_vector(v0)
+    >>> np.allclose(v1, v0 / np.linalg.norm(v0))
+    True
+    >>> v0 = np.random.rand(5, 4, 3)
+    >>> v1 = unit_vector(v0, axis=-1)
+    >>> v2 = v0 / np.expand_dims(np.sqrt(np.sum(v0*v0, axis=2)), 2)
+    >>> np.allclose(v1, v2)
+    True
+    >>> v1 = unit_vector(v0, axis=1)
+    >>> v2 = v0 / np.expand_dims(np.sqrt(np.sum(v0*v0, axis=1)), 1)
+    >>> np.allclose(v1, v2)
+    True
+    >>> v1 = np.empty((5, 4, 3))
+    >>> unit_vector(v0, axis=1, out=v1)
+    >>> np.allclose(v1, v2)
+    True
+    >>> list(unit_vector([]))
+    []
+    >>> list(unit_vector([1]))
+    [1.0]
+
+    """
+    if out is None:
+        data = np.array(data, dtype=np.float64, copy=True)
+        if data.ndim == 1:
+            data /= np.sqrt(np.dot(data, data))
+            return data
+    else:
+        if out is not data:
+            out[:] = np.array(data, copy=False)
+        data = out
+    length = np.atleast_1d(np.sum(data * data, axis))
+    np.sqrt(length, length)
+    if axis is not None:
+        length = np.expand_dims(length, axis)
+    data /= length
+    if out is None:
+        return data
+
+
+def rotation_matrix(angle, direction, point=None):
+    """Return matrix to rotate about axis defined by point and direction.
+
+    >>> R = rotation_matrix(math.pi/2, [0, 0, 1], [1, 0, 0])
+    >>> np.allclose(np.dot(R, [0, 0, 0, 1]), [1, -1, 0, 1])
+    True
+    >>> angle = (np.random.random() - 0.5) * (2*math.pi)
+    >>> direc = np.random.random(3) - 0.5
+    >>> point = np.random.random(3) - 0.5
+    >>> R0 = rotation_matrix(angle, direc, point)
+    >>> R1 = rotation_matrix(angle-2*math.pi, direc, point)
+    >>> is_same_transform(R0, R1)
+    True
+    >>> R0 = rotation_matrix(angle, direc, point)
+    >>> R1 = rotation_matrix(-angle, -direc, point)
+    >>> is_same_transform(R0, R1)
+    True
+    >>> I = np.identity(4, np.float64)
+    >>> np.allclose(I, rotation_matrix(math.pi*2, direc))
+    True
+    >>> np.allclose(2, np.trace(rotation_matrix(math.pi/2,
+    ...                                               direc, point)))
+    True
+
+    """
+    sina = np.sin(angle)
+    cosa = np.cos(angle)
+    direction = unit_vector(direction[:3])
+    # rotation matrix around unit vector
+    R = np.diag([cosa, cosa, cosa])
+    R += np.outer(direction, direction) * (1.0 - cosa)
+    direction *= sina
+    R += np.array([[+0.0, -direction[2], +direction[1]],
+                   [+direction[2], +0.0, -direction[0]],
+                   [-direction[1], +direction[0], +0.0]])
+    M = np.identity(4)
+    M[:3, :3] = R
+    if point is not None:
+        # rotation not around origin
+        point = np.array(point[:3], dtype=np.float64, copy=False)
+        M[:3, 3] = point - np.dot(R, point)
+    return M
+
+
+def Cartesian2Spherical(xyz):
+    ptsnew = np.empty(np.shape(xyz))
+    ptsnew[:, 0] = np.sqrt(xyz[:, 0]**2 + xyz[:, 1]**2 + xyz[:, 2]**2)
+    ptsnew[:, 1] = np.arccos(xyz[:, 2] / np.sqrt(xyz[:, 0]**2 + xyz[:, 1]**2 + xyz[:, 2]**2))  # for elevation angle defined from Z-axis down
+    ptsnew[:, 2] = np.arctan2(xyz[:, 1], xyz[:, 0])
+    return ptsnew
+
+
+def Spherical2Cartesian(rtp):
+    ptsnew = np.empty(np.shape(rtp))
+    ptsnew[:, 0] = rtp[:, 0] * np.sin(rtp[:, 1]) * np.cos(rtp[:, 2])
+    ptsnew[:, 1] = rtp[:, 0] * np.sin(rtp[:, 1]) * np.sin(rtp[:, 2])
+    ptsnew[:, 2] = rtp[:, 0] * np.cos(rtp[:, 1])
+    return ptsnew
 
 
 def run(surfaces, project, Directory, scene, plot=False):

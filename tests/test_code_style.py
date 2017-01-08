5715d6b8350452880cecf77d37420eb1a9d1c8b9
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2017-01-08 22:10:45 +0100

    [master] Updated test for PEP8

--- tests/test_code_style.py
+++ tests/test_code_style.py
@@ -9,7 +9,7 @@ class TestCodeStyle(unittest.TestCase):
 
     def test_pep8_conformance(self):
         tests_dir = os.path.dirname(__file__)
-        modules_files = os.path.abspath(os.path.join(tests_dir, ".."))
+        modules_files = os.path.abspath(os.path.join(tests_dir, "..", "raytracer"))
         pep8style = pep8.StyleGuide(max_line_length=200, ignore=['E731'])
         result = pep8style.check_files([modules_files])
         self.assertEqual(result.total_errors, 0, "Found pep8 conformance issues")
